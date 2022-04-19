47442a01dc8c3a1c48ea854cb1ba0833d933ffef
migasrcf <miguellgomes64@gmail.com>
2022-04-18 22:38:09 -0300

    mostrando messagem de error com o design do projeto e criando mensagem personalizadas para os erros

--- js/validacao.js
+++ js/validacao.js
@@ -4,17 +4,37 @@ export function valida(input) {
   if (validadores[tipoDeInput]) {
     validadores[tipoDeInput](input);
   }
+
+  if (input.validity.valid) {
+    input.parentElement.classList.remove('input-container--invalido');
+  } else {
+    input.parentElement.classList.add('input-container--invalido');
+  }
 }
 
-//!Validacao da data de nascimento nao esta funcionando ***************************
+const messagemDeErro = {
+  nome: {
+    valueMissing: 'O campo nome não pode estar vazio',
+  },
+  email: {
+    valueMissing: 'O campo email não pode estar vazio',
+    typeMismatch: 'O email digitao não eh valido!',
+  },
+  senha: {
+    valueMissing: 'O campo senha não pode estar vazio',
+    patternMismatch:
+      'A senha deve conter no minimo 8 caracteres, pelo menos uma letra maiuscula e minuscula, um numero e um caracter especial',
+  },
+  dataNascimento: {
+    valueMissing: 'O campo data de nascimento não pode estar vazio',
+    customError: 'Voce deve ser maior que 18 para se cadastrar',
+  },
+};
+
+//Validacao da data de nascimento
 const validadores = {
   dataNascimento: (input) => validaDataNascimento(input),
 };
-// const dataNascimento = document.querySelector('#nascimento');
-
-// dataNascimento.addEventListener('blur', (evento) => {
-//   validaDataNascimento(evento.target);
-// });
 
 function validaDataNascimento(input) {
   const dataRecebida = new Date(input.value);
